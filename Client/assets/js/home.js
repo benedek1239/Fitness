29fb8e3ae735a5286405ba96092f9786c388a85c
benedek1239 <benedek.szabolcs98@freemail.hu>
2021-04-21 14:27:06 +0300

    client and admin pages added

---
+++ Client/assets/js/home.js
@@ -0,0 +1,19 @@
+//function to show the main page when signed in as a Client
+function showMainPageSignedInClient(){
+    document.getElementById("user-client").style.display = "inline";
+    document.getElementById("user-admin").style.display = "none";
+    document.getElementById("user-client-text").style.display = "block";
+    document.getElementById("user-admin-text").style.display = "none";
+}
+    
+//function to show the main page when signed in as an Admin
+function showMainPageSignedInAdmin(){
+    document.getElementById("user-client").style.display = "none";
+    document.getElementById("user-admin").style.display = "inline";
+    document.getElementById("user-client-text").style.display = "none";
+    document.getElementById("user-admin-text").style.display = "block";
+}
+
+showMainPageSignedInClient();
+
+
